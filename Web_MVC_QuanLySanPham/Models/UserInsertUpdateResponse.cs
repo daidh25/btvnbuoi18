f1fd9fbd20326affe5757c40040900c10c743ba3
daidh25 <dinhhuydai1234@gmail.com>
2024-05-28 17:04:23 +0700

    btvnbuoi18

---
+++ Web_MVC_QuanLySanPham/Models/UserInsertUpdateResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Web_MVC_QuanLySanPham.Models
+{
+    public class UserInsertUpdateResponse
+    {
+        public int ResponseCode { get; set; }
+        public string ResponseMessage { get; set; }
+    }
+}
